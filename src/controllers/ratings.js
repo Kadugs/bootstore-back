81d2b54a018af97c90f221536bfde227714323bd
Luis Felipe <l_felipelizeu@hotmail.com>
2021-11-10 19:19:22 -0300

    Correcting the style. Adapted to the change of the route name.

--- src/controllers/ratings.js
+++ src/controllers/ratings.js
@@ -1,32 +1,32 @@
 import connection from '../database/database.js';
 
-async function getProductsRatings (req, res) {
-    try {
-        const result = await connection.query("SELECT ratings.rating, sales.product_id FROM ratings JOIN sales ON ratings.sale_id = sales.id;");
+async function getProductsRatings(req, res) {
+  try {
+    const result = await connection.query('SELECT ratings.rating, products.code FROM ratings JOIN sales ON ratings.sale_id = sales.id JOIN products ON sales.product_id = products.id;');
 
-        const ratings = [];
+    const ratings = [];
 
-        let separatedRatings = result.rows;
-        while (separatedRatings.length > 0) {
-            const id = separatedRatings[0].product_id;
-            const productRatings = separatedRatings.filter((rating) => rating.product_id === id);
-            const averageRating = productRatings.map((item) => item.rating).reduce((a, b) => a + b, 0) / productRatings.length;
+    let separatedRatings = result.rows;
+    while (separatedRatings.length > 0) {
+      const productCode = separatedRatings[0].code;
+      const productRatings = separatedRatings.filter((rating) => rating.code === productCode);
+      const averageRating = productRatings.map((item) => item.rating).reduce((a, b) => a + b, 0) / productRatings.length;
 
-            ratings.push({
-                productId: id,
-                average: averageRating,
-                quantity: productRatings.length,
-            });
-            separatedRatings = separatedRatings.filter((rating) => rating.product_id !== id);
-        }
-
-        res.status(200).send(ratings);
-    } catch (error) {
-        console.log(error);
-        res.sendStatus(500);
+      ratings.push({
+        productCode,
+        average: averageRating,
+        quantity: productRatings.length,
+      });
+      separatedRatings = separatedRatings.filter((rating) => rating.code !== productCode);
     }
+
+    res.status(200).send(ratings);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
 }
 
 export {
-    getProductsRatings,
-}
+  getProductsRatings,
+};
