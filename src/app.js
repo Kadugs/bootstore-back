4e7e3bfa9329ffaa1174e61c832e009ecc0d9a3b
Luis Felipe <l_felipelizeu@hotmail.com>
2021-11-12 18:45:50 -0300

    Created a post request to /sign-up, now a visitor can sign up at the app.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import { getProducts, getProductDetails } from './controllers/products.js';
 import { getProductsRatings } from './controllers/ratings.js';
 import { getCart, addToCart } from './controllers/cart.js';
+import { signUp } from './controllers/users.js';
 
 const app = express();
 app.use(cors());
@@ -17,5 +18,7 @@ app.get('/ratings', getProductsRatings);
 app.get('/product/:code', getProductDetails);
 
 app.post('/cart', addToCart);
+
+app.post('/sign-up', signUp);
 
 export default app;
---
+++ src/controllers/users.js
@@ -0,0 +1,25 @@
+import connection from '../database/database.js';
+import { isSignUpDataValid } from '../validation/signUp.js';
+
+async function signUp(req, res) {
+  const { name, cpf, password, email } = req.body;
+
+  if (!isSignUpDataValid(req.body)) return res.sendStatus(400);
+
+  try {
+    await connection.query('INSERT INTO users (name, cpf, password, email) VALUES ($1, $2, $3, $4);', [name, cpf, password, email]);
+
+    return res.sendStatus(201);
+  } catch (error) {
+    console.log(error);
+    const { constraint } = error;
+    if (constraint === 'users_cpf_key') return res.status(409).send('CPF já cadastrado!');
+    if (constraint === 'users_email_key') return res.status(409).send('Email já cadastrado!');
+
+    return res.sendStatus(500);
+  }
+}
+
+export {
+  signUp,
+};
